dc72e33dcfac4a478e9f24d39a318c42cb2e8b8e
Caio Henrique <caiofgv.ch@gmail.com>
2019-04-17 21:23:09 -0300

    primeiro commit

---
+++ javascript/javascript.js
@@ -0,0 +1,28 @@
+//  alert('Hello, world!');
+
+let docePreferido = "pudim";
+docePreferido = "chocolate";
+console.log(docePreferido);
+
+let meuNumero = 20;
+meuNumero = 1.20;
+console.log(meuNumero);
+
+let valido = true;
+console.log('valido1:', valido);
+valido = false;
+console.log('valido2:', valido);
+
+let minhaLista = ["leite condencado","ovo", "creme de leite"];
+console.log(minhaLista.length);
+console.log(minhaLista[1]);
+
+let meuCarro = {
+    marca:"Fiat",
+    modelo:"Siena",
+    cor: "Branco",
+    ano:2010,
+    portas: 4,
+};
+
+console.log(meuCarro);
