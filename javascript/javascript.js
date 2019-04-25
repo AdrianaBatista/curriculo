be14eeee7588d4554146eb53678f04d5ff69ad51
Caio Henrique <caiofgv.ch@gmail.com>
2019-04-24 21:13:07 -0300

    novo commit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -26,3 +26,74 @@ let meuCarro = {
 };
 
 console.log(meuCarro);
+
+//subtração
+let anoAtual = 2019;
+
+console.log(anoAtual);
+
+let anoNascimento = 1982;
+
+console.log(anoNascimento);
+
+let idade = anoAtual - anoNascimento;
+console.log(idade);
+let maiorIdadeBR = 18;
+let maiorIdadeUSA = 37;
+
+console.log('idade:',idade);
+console.log('é maior idade brasil:', idade > maiorIdadeBR);
+console.log(' é maior idade USA:',  idade > maiorIdadeUSA);
+let idoso = idade;
+console.log('é idoso:', idade >= idoso);
+
+// if é se
+// else é se não 
+
+
+/*if(idade >= idoso) {
+    alert("seja bem-vindo!");
+
+} else {
+    alert("Não seja bem-vindo!")
+}*/
+
+
+
+
+
+let produto = 5.99;
+let carteira = 42;
+let vezez = 8; 
+
+let conta = vezez * produto;
+
+console.log(produto * vezez);
+console.log(carteira);
+console.log(conta);
+
+if(carteira >= conta ) {
+   alert("uhul, to rico");
+} else{
+   alert("ixi, não tenho donheiro");
+}
+
+
+
+
+
+console.log(produto * vezez);
+
+console.log(idade);
+
+let roupa1 = "camise";
+let roupa2 = "short";
+
+let frase = roupa1 + " e " + roupa2 + "!";
+
+console.log(frase);
+
+
+
+
+
